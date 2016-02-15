3e9af4890b64f29e978a03987e872b97e26a915f
Itgel Ganbold <itgel.ganbold1@gmail.com>
2016-02-15 18:31:38 +0000

    Refactor 2, and fix exit and entrance same wall then entrance blocked by 3 ones

--- hw1-path-finder/path.c
+++ hw1-path-finder/path.c
@@ -6,6 +6,8 @@
 #define KGRN  "\x1B[32m"
 #define KBLU  "\x1B[34m"
 #define KWHT  "\x1B[37m"
+#define KYLW  "\x1b[33m"
+#define KRED  "\x1b[31m"
 #define RIGHT 'r'
 #define LEFT  'l'
 #define UP    'u'
@@ -14,13 +16,16 @@
 #define ONE   '1'
 #define XMARK 'X'
 
-
+// custom row and column 
 int customM, customN;
+int start1,start2;
 
+// Position type represnts positions on the maze
+// and which direction (DOWN, RIGHT, UP, LEFT) it is going
 struct Position {
    int row;
    int column;
-   int isZeroWall;
+   int isZeroOuterWall;
    char dir;
 };
 
@@ -28,10 +33,18 @@ void findPath(char [][N]);
 void findEntrance(char [][N], struct Position *pos);
 void findExit(char [][N], struct Position *pos); 
 void findZeroWall(char [][N], struct Position *pos,int);
-void findRowEntrance(char [][N], int, int, struct Position *pos);
-void findColumnEntrance(char [][N], int, int, struct Position *pos);
+void findRowEntrance(char [][N], int, int, struct Position *pos,int);
+void findColumnEntrance(char [][N], int, int, struct Position *pos,int);
+void copyPoint(struct Position *pos1, struct Position *pos2);
 void printArray(char [][N]);
+void printNoPathFound();
+void printPathFound(struct Position *pos);
 int isPositionEqual(struct Position *pos1,struct Position *pos2);
+int isEntranceBlocked(char [][N],struct Position *pos);
+int isRightBlocked(char [][N],struct Position *pos);
+int isLeftBlocked(char [][N],struct Position *pos);
+int isUpBlocked(char [][N],struct Position *pos);
+int isDownBlocked(char [][N],struct Position *pos);
 void markVisitedPath(char [][N],struct Position *pos);
 void reverseDirection(struct Position *pos);
 int checkRight(char array[][N],struct Position *pos);
@@ -51,14 +64,16 @@ main()
   char c;
   char file_name[20];
   int i,j;
-  
-  printf("Provide row # and column #: \n");
-  printf("Enter # of row: ");
+  printf("%sWarning: the Field row and column number exactly match with the row and column number you provide.\nOtherwise, the PathFinder app will not work.\n",KYLW);
+  printf("%s",KNRM);
+  printf("Provide the exact row # and column #: \n");
+  printf("Enter the # of row: ");
   scanf("%d", &customM);
-  printf("Enter # of column: ");
+  printf("Enter the # of column: ");
   scanf("%d", &customN);
   
   printf("Type in the name of the file containing the Field\n");
+  printf("Enter the Field name: ");
   scanf("%s",file_name);
   fptr=fopen(file_name,"r");
   for (i=0; i<customM; i++)
@@ -70,6 +85,8 @@ main()
   fclose(fptr);
 
   printArray(array);
+  printf("Before Traverse:\n"); 
+  printf("Traversing ...\n"); 
     
   findPath(array);
 }  
@@ -81,65 +98,117 @@ void findPath(char array[][N]) {
     
     findEntrance(array, &entryPos);
     findExit(array, &exitPos);
-  
-    currentPos.row = entryPos.row;
-    currentPos.column = entryPos.column;
-    currentPos.dir = entryPos.dir;
+    copyPoint(&currentPos,&entryPos);
     
-    while(1) {
-        if(checkRight(array,&currentPos)) {         
-            markVisitedPath(array,&currentPos);     
-            turnRight(array,&currentPos);                 
-            changeDirection(&currentPos);
-        } else if(checkForward(array,&currentPos)) {
-            markVisitedPath(array,&currentPos);     
-            goForward(&currentPos);                     
-        } else {
-            reverseDirection(&currentPos);          
-            continue;
+    printf("* Entrance @ row: %d column: %d \n",entryPos.row,entryPos.column);
+    printf("* Exit @ row: %d column: %d \n",exitPos.row,exitPos.column);
+    
+    if(isEntranceBlocked(array,&entryPos)) {
+        markVisitedPath(array,&entryPos); 
+        printArray(array);
+        printNoPathFound();
+    } else {
+    
+        while(1) {
+            if(checkRight(array,&currentPos)) {         
+                markVisitedPath(array,&currentPos);     
+                turnRight(array,&currentPos);                 
+                changeDirection(&currentPos);
+            } else if(checkForward(array,&currentPos)) {
+                markVisitedPath(array,&currentPos);     
+                goForward(&currentPos);                     
+            } else {
+                reverseDirection(&currentPos);          
+                continue;
+            }
+            
+            if(isPositionEqual(&currentPos, &entryPos)) {
+                printArray(array);
+                printNoPathFound();
+                break;
+            } else if(isPositionEqual(&currentPos, &exitPos)) {
+                markVisitedPath(array,&currentPos);
+                printArray(array);
+                printPathFound(&currentPos);
+                break;
+            }  
         }
-        
-        if(isPositionEqual(&currentPos, &entryPos)) {
-            printf(" :( No exit");
-            printArray(array);
-            break;
-        } else if(isPositionEqual(&currentPos, &exitPos)) {
-            printf("\n****************\n");
-            printf("*      WOW     *\n");
-            printf("*  Found exit  *\n");
-            printf("* @ r: %d c: %d  *\n",currentPos.row,currentPos.column);
-            printf("****************\n");
-            markVisitedPath(array,&currentPos);
-            printArray(array);
-            break;
-        }  
     }
-    
-    printf("currentPos column: %d row: %d",currentPos.column,currentPos.row);
-    printf("\n");
+}
+
+void printNoPathFound() {
+    printf("After Traverse: %s:( No path found\n\n",KRED);
+    printf("%s",KNRM);    
+}
+
+void printPathFound(struct Position *pos) {
+    printf("After Traverse:\n\n");
+    printf("\n****************\n");
+    printf("*      WOW     *\n");
+    printf("*  Path found  *\n");
+    printf("* @ r: %d c: %d  *\n",pos->row,pos->column);
+    printf("****************\n");
+}
+
+void copyPoint(struct Position *pos1, struct Position *pos2) {
+    pos1->row = pos2->row;
+    pos1->column = pos2->column;
+    pos1->dir = pos2->dir;
+}
+
+// Checks Entrance Position blocked by three ones
+int isEntranceBlocked(char array[][N],struct Position *pos) {
+    switch(pos->dir) {
+        case DOWN:
+            return (isRightBlocked(array,pos) && isLeftBlocked(array,pos) && isDownBlocked(array,pos));
+        case UP:
+            return (isRightBlocked(array,pos) && isLeftBlocked(array,pos) && isUpBlocked(array,pos));
+        case LEFT:
+            return (isUpBlocked(array,pos) && isLeftBlocked(array,pos) && isDownBlocked(array,pos));
+        case RIGHT:
+            return (isRightBlocked(array,pos) && isUpBlocked(array,pos) && isDownBlocked(array,pos));
+    }
+}
+
+int isRightBlocked(char array[][N],struct Position *pos) {
+    return (array[pos->row][pos->column+1] == ONE);   
+}
+
+int isLeftBlocked(char array[][N],struct Position *pos) {
+    return (array[pos->row][pos->column-1] == ONE);   
+}
+
+int isUpBlocked(char array[][N],struct Position *pos) {
+    return (array[pos->row-1][pos->column] == ONE);   
+}
+
+int isDownBlocked(char array[][N],struct Position *pos) {
+    return (array[pos->row+1][pos->column] == ONE);   
 }
 
 // finding the entrance 
 void findEntrance(char array[][N], struct Position *pos) {
-    pos->isZeroWall = 0;
-    
+    pos->isZeroOuterWall = 0;
+    start1=1;
+    start2=0;
     int i;
     for(i=0; i< 4;i++)
     {
         findZeroWall(array,pos,i);
-        if(pos->isZeroWall) break;
+        if(pos->isZeroOuterWall) break;
     }
 }
 
 // finding the exit
 void findExit(char array[][N], struct Position *pos) {
-    pos->isZeroWall = 0;
-    
+    pos->isZeroOuterWall = 0;
+    start1=0;
+    start2=1;
     int i;
     for(i=3; i>= 0;i--)
     {
         findZeroWall(array,pos,i);
-        if(pos->isZeroWall) break;
+        if(pos->isZeroOuterWall) break;
     }
     
 }
@@ -149,23 +218,23 @@ void findZeroWall(char array[][N], struct Position *pos,int wall) {
     switch(wall) {
         case 0:
             // finding zero from first top row
-            findRowEntrance(array,0,customN,pos);
+            findRowEntrance(array,0,customN,pos,start1);
             break;
         case 1:
             // finding entrance from last column
-            findColumnEntrance(array,(customN-1),customM,pos);
+            findColumnEntrance(array,(customN-1),customM,pos,start1);
             break;
         case 2:
             // finding entrance from last bottow row
-            findRowEntrance(array,(customM-1),customN,pos);
+            findRowEntrance(array,(customM-1),customN,pos,start2);
             break;
         case 3:
             // finding entrance from first column
-            findColumnEntrance(array,0,customM,pos);
+            findColumnEntrance(array,0,customM,pos,start2);
             break;
     }
         
-    if(pos->isZeroWall) {
+    if(pos->isZeroOuterWall) {
         switch(wall) {
             case 0:
                 pos->dir = DOWN;
@@ -184,31 +253,56 @@ void findZeroWall(char array[][N], struct Position *pos,int wall) {
 }
 
 // finds '0' from given wall(row)
-void findRowEntrance(char array[][N],int row, int size, struct Position *pos) {
+void findRowEntrance(char array[][N],int row, int size, struct Position *pos,int start) {
     int i;
-    for(i=0;i<size;i++) {
-     if(array[row][i] == ZERO)
-     {
-         pos->isZeroWall = 1;
-         pos->row = row;
-         pos->column = i;
-         break;
-     }
-    } 
+    if(start) {
+        for(i=0;i<size;i++) {
+         if(array[row][i] == ZERO)
+         {
+             pos->isZeroOuterWall = 1;
+             pos->row = row;
+             pos->column = i;
+             break;
+         }
+        }
+    } else {
+        for(i=(size-1); i >= 0 ; i--) {
+         if(array[row][i] == ZERO)
+         {
+             pos->isZeroOuterWall = 1;
+             pos->row = row;
+             pos->column = i;
+             break;
+         }
+        }
+    }
 }
 
 // finds '0' from given wall(column)
-void findColumnEntrance(char array[][N], int column, int size, struct Position *pos) 
+void findColumnEntrance(char array[][N], int column, int size, struct Position *pos,int start) 
 {
     int i;
-    for(i=0;i<size;i++) {
-      if(array[i][column] == ZERO)
-      {
-        pos->isZeroWall = 1;
-        pos->row = i;
-        pos->column = column;
-        break;
-      }
+    if(start)
+    {
+        for(i=0;i<size;i++) {
+          if(array[i][column] == ZERO)
+          {
+            pos->isZeroOuterWall = 1;
+            pos->row = i;
+            pos->column = column;
+            break;
+          }
+        }
+    } else {
+        for(i=(size-1); i>= 0; i--) {
+          if(array[i][column] == ZERO)
+          {
+            pos->isZeroOuterWall = 1;
+            pos->row = i;
+            pos->column = column;
+            break;
+          }
+        }
     }
 }
 
